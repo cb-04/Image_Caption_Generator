3c87f7ffc01bf1a1f823978ceeff9c0639b8b476
Chandrabali Biswas <chandrabalibiswas04@gmail.com>
2025-12-24 23:09:13 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import string
+import numpy as np
+import os
+from pickle import dump,load
+import matplotlib.pyplot as plt
+
+def load_doc(filename):
+    file = open(filename,'r')
+    text = file.read()
+    file.close()
+    return text
+
+def all_image_captions(filename):
+    file = load_doc(filename)
+    captions = file.split('\n')
+    descriptions = {}
+    for caption in captions[:-1]:
+        img, caption = caption.split('\t')
+        if img[:-2] not in descriptions:
+            descriptions[img[:-2]] = [caption]
+        else:
+            descriptions[img[:-2]].append(caption)
+    return descriptions
+
+def cleaning_text(captions):
+    table = str.maketrans('', '', string.punctuation)
+    for img, caps in captions.items():
+        for i, img_caption in enumerate(caps):
+            img_caption.replace("-","")
+            desc = img_caption.split()
+            desc = [word.lower() for word in desc]
+            desc = [word.translate(table) for word in desc]
+            desc = [word for word in desc if (len(word))>1]
+            desc = [word for word in desc if(word.isalpha())]
+
+            img_caption = ' '.join(desc)
+            captions[img][i] = img_caption
+    return captions
