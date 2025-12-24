5ada4e9db78da9920c878eacc019ce7db9d01a5c
Chandrabali Biswas <chandrabalibiswas04@gmail.com>
2025-12-24 23:59:30 +0530

    cleaned and preprocessed dataset

--- main.py
+++ main.py
@@ -52,3 +52,16 @@ def save_descriptions(descriptions, filename):
     file = open(filename,'w')
     file.write(data)
     file.close()
+
+dataset_text = "Flickr8k_text"
+dataset_images = "Flicker8k_Dataset"
+
+filename = dataset_text + "/Flickr8k.token.txt"
+descriptions = all_image_captions(filename)
+print('Length of descriptions =',len(descriptions))
+
+clean_descriptions = cleaning_text(descriptions)
+vocabulary = text_vocabulary(clean_descriptions)
+print('Length of vocabulary =',len(vocabulary))
+
+save_descriptions(clean_descriptions,"descriptions.txt")
